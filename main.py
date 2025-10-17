5eee0981366732f244621a4ec9961e3acd156f76
rafailvv <r.venediktov@innopolis.university>
2025-10-17 19:35:36 +0200

    update port

--- main.py
+++ main.py
@@ -244,4 +244,4 @@ async def export_pdf(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
